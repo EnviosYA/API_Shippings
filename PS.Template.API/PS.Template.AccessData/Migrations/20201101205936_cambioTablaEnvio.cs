a99297f0b9bf94cc1ffed945dbf91585950290f4
damidj92 <damidj92@gmail.com>
2020-11-01 18:17:25 -0300

    Cambios de columnas en la tabla Envio

---
+++ PS.Template.API/PS.Template.AccessData/Migrations/20201101205936_cambioTablaEnvio.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PS.Template.AccessData.Migrations
+{
+    public partial class cambioTablaEnvio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "idSucDestino",
+                table: "Envio");
+
+            migrationBuilder.DropColumn(
+                name: "idSucOrigen",
+                table: "Envio");
+
+            migrationBuilder.DropColumn(
+                name: "idUserDestino",
+                table: "Envio");
+
+            migrationBuilder.AddColumn<int>(
+                name: "idDireccionDestino",
+                table: "Envio",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "idDireccionDestino",
+                table: "Envio");
+
+            migrationBuilder.AddColumn<int>(
+                name: "idSucDestino",
+                table: "Envio",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "idSucOrigen",
+                table: "Envio",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "idUserDestino",
+                table: "Envio",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
