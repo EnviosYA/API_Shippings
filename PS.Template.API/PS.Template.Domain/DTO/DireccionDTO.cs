a93b4766dff2985b0838a0e23edd2834acf607f9
cirogargatagli <ciroshaila@gmail.com>
2020-11-26 03:13:11 -0300

    Cambie tipo latitud y long por double

--- PS.Template.API/PS.Template.Domain/DTO/DireccionDTO.cs
+++ PS.Template.API/PS.Template.Domain/DTO/DireccionDTO.cs
@@ -3,8 +3,8 @@ namespace PS.Template.Domain.DTO
 {
     public class DireccionDTO
     {
-        public string Latitud { get; set; }
-        public string Longitud { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
         public string Calle { get; set; }
         public int Altura { get; set; }
         public int IdLocalidad { get; set; }
